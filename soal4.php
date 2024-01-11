25d972f15366cc87186629da23410ce5ebfbd82f
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2024-01-11 20:18:12 +0700

    UAP Web

---
+++ soal4.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     
+</head>
+<body>
+    <p style="color: blue">Paragraf ini berwarna biru</p>
+    
+    <div style="margin: 100px 0; font-family: Arial, Helvetica, sans-serif">
+    <a style="margin: 0 10px; color: black;" href="soal1.php">Soal 1</a>
+    <a style="margin: 0 10px; color: black;" href="soal2.php">Soal 2</a>
+    <a style="margin: 0 10px; color: black;" href="soal3.php">Soal 3</a>
+    <a style="margin: 0 10px; color: black;" href="soal4.php">Soal 4</a>
+    <a style="margin: 0 10px; color: black;" href="soal5.php">Soal 5</a>
+    <a style="margin: 0 10px; color: black;" href="soal6.php">Soal 6</a>
+    <a style="margin: 0 10px; color: black;" href="soal7.php">Soal 7</a>
+    <a style="margin: 0 10px; color: black;" href="soal8.php">Soal 8</a>
+    <a style="margin: 0 10px; color: black;" href="soal9.php">Soal 9</a>
+    <a style="margin: 0 10px; color: black;" href="soal10.php">Soal 10</a>
+    <a style="margin: 0 10px; color: black;" href="soal11.php">Soal 11</a>
+    <a style="margin: 0 10px; color: black;" href="soal12.php">Soal 12</a>
+    <a style="margin: 0 10px; color: black;" href="soal13.php">Soal 13</a>
+    <a style="margin: 0 10px; color: black;" href="soal14.php">Soal 14</a>
+    <a style="margin: 0 10px; color: black;" href="soal15.php">Soal 15</a>
+</div>
+</body>
+</html>
